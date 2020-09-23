be3df2408d4f695a49fcf4b8a29be6d79b5d20a0
vlele <vishwas.lele@appliedis.com>
2020-09-23 16:33:29 +0530

    Update snippets.sh

--- m10/snippets.sh
+++ m10/snippets.sh
@@ -37,13 +37,15 @@ kubectl describe pv pvc-1b5f6874-9561-405c-92d0-2412aaa1ea4e
 # Shared volumes must be of the Azure File type. Azure files only work with Standard storage. They will fail if you try using Premium.
 # In most cases persistent _disk_ storage is only available in a one-to-one mapping connecting a disk to a pod directly. 
 # In AKS, it is also possible to map _file_ style resources, which use the SMB storage model, and allow multiple connections and multiple read and write resources.  In order to use the file storage resources, we first need to create a storage account if we don't have one already, and it needs to be associated with our AKS environment.  Find our node resource class name:
-az aks show --resource-group $RESOURCE_GROUP_NAME --name $CLUSTER_NAME --query nodeResourceGroup 
+az aks show -g $RESOURCE_GROUP_NAME -n $CLUSTER_NAME --query nodeResourceGroup
+
+MC_RESOURCE_GROUP_NAME="<Paste-Resource-Group-Name-From-Above-Command-Output>"
 
 # Then we can create a storage account with that resource group (note that the group should start with MC_):
 az storage account create -n $STORAGE_NAME -g $MC_RESOURCE_GROUP_NAME -l $LOCATION --sku Standard_LRS
 
 # Now we can create the storage class, and a role and role binding which are needed to allow the storage controller to create a secret for the PVC to use for accessing the file share resource.
-> Edit the "manifests/file_sc.yml" file to provide the correct value for "storageAccount: $storage_name"
+> Edit the "manifests/file_sc.yml" file to provide the correct value for "storageAccount: $STORAGE_NAME"
 
 # Create the storage class:
 kubectl apply -f manifests/file_sc.yml
@@ -60,7 +62,7 @@ kubectl get pvc aksclassazurefilepvc -w
 # And if we attach the PVC to a pod as we did previously, we can attach more than one POD to the same file resource:
 kubectl apply -f manifests/hostname_file.yml
 
-# And we can now write into, and read from the two pods to see that the both mount the same filesystem:
+# And we can now write into one pod and read from other pod to see that the both mount the same file system
 kubectl exec -it $(kubectl get pod -l app=hostname-file -o jsonpath='{.items[0].metadata.name}') -- sh -c 'hostname > /www/hostname; cat /www/hostname'
 
 kubectl exec -it $(kubectl get pod -l app=hostname-file -o jsonpath='{.items[1].metadata.name}') -- sh -c 'cat /www/hostname'
