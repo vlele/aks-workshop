cbeedce945ac1b51f11fb4e3476416a42e675b27
vlele <vishwas.lele@appliedis.com>
2020-09-24 19:33:37 +0530

    Update snippet.sh

--- m16/snippet.sh
+++ m16/snippet.sh
@@ -1,5 +1,7 @@
 #***************************************************************************************
 ## Objective: This module demonstrates the network policy in AKS. It creates a MySql Pod and a  MySql Client Pod. First we show 
+## Note: This demo is not verifiable due to "https://github.com/kubernetes/kubernetes/issues/90077" where it says "No matches for kind "Ingress" in 
+## version "networking.k8s.io/v1". 
 #***************************************************************************************
 
 NAMESPACE=mysqlhelm
@@ -13,11 +15,13 @@ cd ../m16
 
 # apply network policy
 kubectl apply -f manifests/backend-policy-disallow.yaml
-
+kubectl describe networkpolicy.networking.k8s.io/backend-policy
 # switch to ubuntu ssh and reconnect to the my sql instance 
 
 kubectl apply -f manifests/backend-policy-allow.yaml
+kubectl describe networkpolicy.networking.k8s.io/backend-policy
 
 # Cleanup Steps:
-helm del --purge MYSQL_HELM_PACKAGE_NAME
+#helm del --purge MYSQL_HELM_PACKAGE_NAME    <-- This will work in helm version 2
+helm uninstall $MYSQL_HELM_PACKAGE_NAME
 kubectl delete namespace $NAMESPACE
